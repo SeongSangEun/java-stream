1c5224349867868fb4453b3c49bdd93b68fae2d3
SeongSangEun <tjdtkddms@gmail.com>
2023-08-04 08:17:17 +0900

    Part3 - BiFunction<T, U, R> 매개변수가 3개인 기본 함수인터페이스 사용

--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,22 +1,12 @@
 import util.Adder;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-//        Function<Integer, Integer> myAdder = new Adder();
-//        Integer result = myAdder.apply(10);
-//        System.out.println("result = " + result);
-
-//        Function<Integer, Integer> myAdder = (Integer x) ->{
-//            return x + 10;
-//        };
-
-        //간락하게 만들기
-        // 앞에 InputType을 Integer로 선언을 하였기 때문에 매개변수 내 Integer를 새략이 가능하다.
-        Function<Integer, Integer> myAdder = x -> x + 10;
-
-        Integer result = myAdder.apply(10);
-        System.out.println("result = " + result);
+        BiFunction<Integer, Integer, Integer> add = (x, y) -> x + y;
+        Integer result = add.apply(3, 5);
+        System.out.println("apply = " + result);
     }
 }
