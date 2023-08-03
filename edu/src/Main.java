df0ecb1143dacaa750429d53a67de059cee90287
SeongSangEun <tjdtkddms@gmail.com>
2023-08-04 08:23:57 +0900

    Part3 - 새로운 인터페이스 FunctionalInterface 구현

--- edu/src/Main.java
+++ edu/src/Main.java
@@ -1,12 +1,15 @@
 import util.Adder;
+import util.TriFunction;
 
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
-        BiFunction<Integer, Integer, Integer> add = (x, y) -> x + y;
-        Integer result = add.apply(3, 5);
-        System.out.println("apply = " + result);
+
+        TriFunction<Integer, Integer, Integer, Integer> addThreeNumbers = (x, y, z) -> x+y+z;
+        Integer result = addThreeNumbers.apply(1, 2, 3);
+        System.out.println("result = " + result);
+
     }
 }
